a95ebcd56fc0c9f99be231fbfbee0a94c586dbd0
dannypsnl <dannypsnl@gmail.com>
2020-01-27 20:53:52 +0800

    Library

---
+++ synthesizing/src/Library.h
@@ -0,0 +1,15 @@
+//
+// Created by dannypsnl on 2020/1/27.
+//
+
+#ifndef SYNTHESIZING_SRC_LIBRARY_H_
+#define SYNTHESIZING_SRC_LIBRARY_H_
+
+#include <vector>
+#include "Component.h"
+
+class Library {
+  std::vector<Component> components;
+};
+
+#endif  // SYNTHESIZING_SRC_LIBRARY_H_
